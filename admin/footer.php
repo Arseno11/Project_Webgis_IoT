9c5818f1fb48dfff9732709e12a2a3eb2b4e0171
Arseno <124229977+Arseno11@users.noreply.github.com>
2023-04-13 03:35:37 +0700

    asd

--- admin/footer.php
+++ admin/footer.php
@@ -1,15 +1,15 @@
 <!-- Footer -->
 <footer>
-  <div class="footer clearfix mb-0 text-muted">
-    <div class="float-start">
-      <p> Copyright &copy;<script>
-        document.write(new Date().getFullYear());
-        </script> Arseno</p>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p> Copyright &copy;<script>
+                    document.write(new Date().getFullYear());
+                </script> Arseno</p>
+        </div>
+        <div class="float-end">
+            <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a href="#">Arseno</a></p>
+        </div>
     </div>
-    <div class="float-end">
-      <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a href="#">Arseno</a></p>
-    </div>
-  </div>
 </footer>
 <!-- End of Footer -->
 
@@ -17,39 +17,9 @@
 <script src="assets/js/app.js"></script>
 
 <!-- dataTables -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"
-  integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ=="
-  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.datatables.net/v/bs5/dt-1.12.1/datatables.min.js"></script>
 <script src="assets/js/pages/datatables.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"
-  integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ=="
-  crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-<script>
-// Add SweetAlert confirmation dialog to delete link
-var deleteLinks = document.querySelectorAll('.delete-link');
-deleteLinks.forEach(function(link) {
-  link.addEventListener('click', function(event) {
-    event.preventDefault();
-
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        // If user confirms delete, redirect to delete script
-        window.location.href = link.href;
-
-      }
-    })
-  });
-});
-</script>
