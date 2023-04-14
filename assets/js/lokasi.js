e92276add7b23732a9480121cf0d6387c377dfc0
Arseno <124229977+Arseno11@users.noreply.github.com>
2023-04-14 19:26:17 +0700

    update

--- assets/js/lokasi.js
+++ assets/js/lokasi.js
@@ -242,11 +242,21 @@ navigator.geolocation.getCurrentPosition(function (location) {
   refreshData();
 });
 
+//notification function
+var notification = L.control
+  .notifications({
+    timeout: 3000,
+    position: 'bottomleft',
+    closable: true,
+    dismissable: true,
+  })
+  .addTo(map);
+
 
 // Menampilkan SweetAlert ketika halaman pertama kali dibuka
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   if (localStorage.getItem('showAlert') !== 'false') {
-    showAlert('success', 'Selamat Datang', 'Ini adalah halaman deteksi dini banjir');
+     notification.success('Selamat Datang: DiWebsite Pedeteksi Dini Banjir');
   }
   localStorage.removeItem('showAlert');
 });
@@ -293,17 +303,16 @@ function updateData() {
         for (let i = 0; i < data.results.length; i++) {
           let result = data.results[i];
           let siaga, hujan;
-
           if (result.jarak <= 10) {
             siaga = '<td style=color:red>Siaga 1</td>';
             if (localStorage.getItem('showAlert_' + result.id_alat) !== 'false') {
-              showAlert('warning', 'Peringatan Banjir', 'Jarak sensor telah mencapai Siaga 1 untuk Alat Dengan Nama ' + result.nama_alat);
+              notification.alert('Peringatan Banjir', 'Jarak sensor telah mencapai Siaga 1 untuk Alat Dengan Nama ' + result.nama_alat);
               localStorage.setItem('showAlert_' + result.id_alat, 'false');
             }
           } else if (result.jarak > 10 && result.jarak <= 25) {
             siaga = '<td style=color:yellow>Siaga 2</td>';
             if (localStorage.getItem('showAlert_' + result.id_alat) !== 'false') {
-              showAlert('info', 'Peringatan Banjir', 'Jarak sensor telah mencapai Siaga 2 untuk Alat Dengan Nama ' + result.nama_alat);
+              notification.warning('Peringatan Banjir', 'Jarak sensor telah mencapai Siaga 2 untuk Alat Dengan Nama ' + result.nama_alat);
               localStorage.setItem('showAlert_' + result.id_alat, 'false');
             }
           } else {
