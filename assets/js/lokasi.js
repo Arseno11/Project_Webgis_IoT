0d7810ae3112f6d63520f45ddd8e0fce41b01f15
Arseno <124229977+Arseno11@users.noreply.github.com>
2023-04-14 18:06:28 +0700

    update

--- assets/js/lokasi.js
+++ assets/js/lokasi.js
@@ -11,6 +11,11 @@ navigator.geolocation.getCurrentPosition(function (location) {
       zoom: 13,
       scrollWheelZoom: false // Menonaktifkan zoom dengan scroll
     });
+    
+    map.on('load', function () {
+      document.getElementById('loader').style.display = 'none';
+    });
+  }, 3000);
 
     // Tambahkan layer base map dari Google Maps
     L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
@@ -19,240 +24,233 @@ navigator.geolocation.getCurrentPosition(function (location) {
     }).addTo(map);
 
 
+  var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+  });
 
-    var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-    });
+  var googleSatelliteLayer = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    detectRetina: true
+  });
 
-    var googleSatelliteLayer = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
-      maxZoom: 20,
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-      detectRetina: true
-    });
-
-    var googleStreetsLayer = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-      maxZoom: 20,
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-      detectRetina: true
-    });
-
-
-    var populationLayer = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-    });
-
-    var bordersLayer = L.tileLayer('https://tile-{s}.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://hot.openstreetmap.org/">Humanitarian OpenStreetMap Team</a>'
-    });
+  var googleStreetsLayer = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    detectRetina: true
+  });
 
 
-    var baseMaps = {
-      "OpenStreetMap": osmLayer,
-      "Google Satellite": googleSatelliteLayer,
-      "Google Streets": googleStreetsLayer,
-    };
+  var populationLayer = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+  });
 
-    var overlayMaps = {
-      "Population Density": populationLayer,
-      "Borders": bordersLayer,
-    };
-
-    L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-    const legend = L.control.Legend({
-      title: "Status Icon",
-      position: "bottomright",
-      collapsed: true, // Set default state to collapsed
-      symbolWidth: 30,
-      opacity: 1,
-      column: 2,
-      legends: [{
-        label: "Bahaya",
-        type: "image",
-        url: "img/bahaya.png",
-      }, {
-        label: "Awas",
-        type: "image",
-        url: "img/awas.png"
-      }, {
-        label: "Aman",
-        type: "image",
-        url: "img/aman.png"
-      }]
-    }).addTo(map);
-
-    var lc = L.control.locate({
-      position: 'topleft',
-      icon: 'fa fa-user', // menggunakan ikon map-marker
-      iconLoading: 'fa fa-spinner fa-spin',
-      setView: 'once',
-      locateOptions: {
-        maxZoom: 30,
-        enableHighAccuracy: true
-      },
-      style: {
-        // mengubah gaya ikon untuk membuatnya lebih besar dan berwarna biru seperti Google
-        fillColor: '#4285F4',
-        fillOpacity: 0.8,
-        stroke: false,
-        radius: 10
-      },
-      iconSize: [50, 50]
-    }).addTo(map);
+  var bordersLayer = L.tileLayer('https://tile-{s}.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://hot.openstreetmap.org/">Humanitarian OpenStreetMap Team</a>'
+  });
 
 
-    // deklarasi variabel untuk menyimpan popup yang sedang terbuka
-    let currentPopup = null;
+  var baseMaps = {
+    "OpenStreetMap": osmLayer,
+    "Google Satellite": googleSatelliteLayer,
+    "Google Streets": googleStreetsLayer,
+  };
+
+  var overlayMaps = {
+    "Population Density": populationLayer,
+    "Borders": bordersLayer,
+  };
+
+  L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+  const legend = L.control.Legend({
+    title: "Status Icon",
+    position: "bottomright",
+    collapsed: true, // Set default state to collapsed
+    symbolWidth: 30,
+    opacity: 1,
+    column: 2,
+    legends: [{
+      label: "Bahaya",
+      type: "image",
+      url: "img/bahaya.png",
+    }, {
+      label: "Awas",
+      type: "image",
+      url: "img/awas.png"
+    }, {
+      label: "Aman",
+      type: "image",
+      url: "img/aman.png"
+    }]
+  }).addTo(map);
+
+  var lc = L.control.locate({
+    position: 'topleft',
+    icon: 'fa fa-user', // menggunakan ikon map-marker
+    iconLoading: 'fa fa-spinner fa-spin',
+    setView: 'once',
+    locateOptions: {
+      maxZoom: 30,
+      enableHighAccuracy: true
+    },
+    style: {
+      // mengubah gaya ikon untuk membuatnya lebih besar dan berwarna biru seperti Google
+      fillColor: '#4285F4',
+      fillOpacity: 0.8,
+      stroke: false,
+      radius: 10
+    },
+    iconSize: [50, 50]
+  }).addTo(map);
+
+  // deklarasi variabel untuk menyimpan popup yang sedang terbuka
+  let currentPopup = null;
 
 
-    // fungsi untuk memanggil data dari API
-    async function loadData() {
-      try {
-        const response = await fetch('ambildata.php');
-        if (!response.ok) {
-          throw new Error('Terjadi kesalahan saat memuat data.');
-        }
-        const data = await response.json();
-
-        // memformat data menjadi objek yang diinginkan
-        const deviceLocations = data.results.map(item => ({
-          id: item.id_alat,
-          name: item.nama_alat,
-          address: item.alamat,
-          jarak: item.jarak,
-          rainfall: item.hujan,
-          longitude: item.longitude,
-          latitude: item.latitude
-        }));
-
-        // Menghapus marker yang telah ada sebelumnya
-        for (let i = 0; i < markers.length; i++) {
-          map.removeLayer(markers[i]);
-        }
-        markers = [];
-
-
-        // Menutup popup yang sedang terbuka jika ada
-        if (currentPopup) {
-          currentPopup._close();
-        }
-
-        // memanggil fungsi untuk menampilkan marker di peta
-        showMarkers(deviceLocations);
-      } catch (error) {
-        console.error(error);
+  // fungsi untuk memanggil data dari API
+  async function loadData() {
+    try {
+      const response = await fetch('ambildata.php');
+      if (!response.ok) {
+        throw new Error('Terjadi kesalahan saat memuat data.');
       }
+      const data = await response.json();
+
+      // memformat data menjadi objek yang diinginkan
+      const deviceLocations = data.results.map(item => ({
+        id: item.id_alat,
+        name: item.nama_alat,
+        address: item.alamat,
+        jarak: item.jarak,
+        rainfall: item.hujan,
+        longitude: item.longitude,
+        latitude: item.latitude
+      }));
+
+      // Menghapus marker yang telah ada sebelumnya
+      for (let i = 0; i < markers.length; i++) {
+        map.removeLayer(markers[i]);
+      }
+      markers = [];
+
+
+      // Menutup popup yang sedang terbuka jika ada
+      if (currentPopup) {
+        currentPopup._close();
+      }
+
+      // memanggil fungsi untuk menampilkan marker di peta
+      showMarkers(deviceLocations);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    // fungsi untuk menampilkan marker di peta
-    function showMarkers(deviceLocations) {
+  // fungsi untuk menampilkan marker di peta
+  function showMarkers(deviceLocations) {
 
-      // Membuat loop untuk setiap lokasi alat
-      for (var i = 0; i < deviceLocations.length; i++) {
-        var deviceLocation = deviceLocations[i];
+    // Membuat loop untuk setiap lokasi alat
+    for (var i = 0; i < deviceLocations.length; i++) {
+      var deviceLocation = deviceLocations[i];
 
-        // Memeriksa status hujan dan jarak air
-        // Memeriksa status hujan dan jarak air
-        let iconUrl;
-        let status;
-        let currentCircle = null; // inisialisasi variabel
+      // Memeriksa status hujan dan jarak air
+      // Memeriksa status hujan dan jarak air
+      let iconUrl;
+      let status;
+      let currentCircle = null; // inisialisasi variabel
 
-        if (deviceLocation.jarak <= 10) {
-          // Menentukan ikon marker dan konten popup
-          iconUrl = 'img/bahaya.png';
-          status = 'Bahaya';
-        } else if (deviceLocation.jarak <= 25) {
-          iconUrl = 'img/awas.png';
-          status = 'Awas';
-        } else {
-          iconUrl = 'img/aman.png';
-          status = 'Aman';
-        }
+      if (deviceLocation.jarak <= 10) {
+        // Menentukan ikon marker dan konten popup
+        iconUrl = 'img/bahaya.png';
+        status = 'Bahaya';
+      } else if (deviceLocation.jarak <= 25) {
+        iconUrl = 'img/awas.png';
+        status = 'Awas';
+      } else {
+        iconUrl = 'img/aman.png';
+        status = 'Aman';
+      }
 
-        // Membuat marker di peta
-        var marker = L.marker([deviceLocation.latitude, deviceLocation.longitude], {
-          title: deviceLocation.name,
-          icon: L.icon({
-            iconUrl: iconUrl,
-            iconSize: [30, 45], // ukuran ikon
-            popupAnchor: [-1, -20]
-          })
-        });
+      // Membuat marker di peta
+      var marker = L.marker([deviceLocation.latitude, deviceLocation.longitude], {
+        title: deviceLocation.name,
+        icon: L.icon({
+          iconUrl: iconUrl,
+          iconSize: [30, 45], // ukuran ikon
+          popupAnchor: [-1, -20]
+        })
+      });
 
-        // Menambahkan popup ke marker
-        var popupContent =
-          `<h6> Nama Alat: ${deviceLocation.name}</h6> 
+      // Menambahkan popup ke marker
+      var popupContent =
+        `<h6> Nama Alat: ${deviceLocation.name}</h6> 
       <h6><p> Status: ${status}</p></h6>
       <h6>Jarak Air: ${deviceLocation.jarak} cm</h6></br>
       <a class='btn btn-success btn-sm' href='detail.php?id_alat=${deviceLocation.id}'> Info Detail </a>
       <a class='btn btn-warning btn-sm' target='_blank' href='https://www.google.com/maps/dir/?api=1&origin=${location.coords.latitude},${location.coords.longitude}&destination=${deviceLocation.latitude},${deviceLocation.longitude}&travelmode=driving'>Rute</a>`;
 
-        // Menambahkan konten popup ke marker
-        marker.bindPopup(popupContent);
-        // Menambahkan marker ke peta dan ke array markers
-        marker.addTo(map);
-        markers.push(marker);
-        // Menambahkan event listener mouseover pada marker
-        marker.on('click', function () {
-          this.openPopup();
-        });
+      // Menambahkan konten popup ke marker
+      marker.bindPopup(popupContent);
+      // Menambahkan marker ke peta dan ke array markers
+      marker.addTo(map);
+      markers.push(marker);
+      // Menambahkan event listener mouseover pada marker
+      marker.on('click', function () {
+        this.openPopup();
+      });
+    }
+  }
+
+  // Array untuk menyimpan marker
+  markers = [];
+
+  // fungsi untuk memanggil data dari API
+  async function loadData() {
+    try {
+      const response = await fetch('ambildata.php');
+      if (!response.ok) {
+        throw new Error('Terjadi kesalahan saat memuat data.');
       }
-    }
+      const data = await response.json(); // memformat data menjadi objek yang diinginkan
+      const deviceLocations = data.results.map(item => ({
+        id: item.id_alat,
+        name: item.nama_alat,
+        address: item.alamat,
+        jarak: item.jarak,
+        rainfall: item.hujan,
+        longitude: item.longitude,
+        latitude: item.latitude
+      }));
 
-    // Array untuk menyimpan marker
-    markers = [];
-
-    // fungsi untuk memanggil data dari API
-    async function loadData() {
-      try {
-        const response = await fetch('ambildata.php');
-        if (!response.ok) {
-          throw new Error('Terjadi kesalahan saat memuat data.');
-        }
-        const data = await response.json(); // memformat data menjadi objek yang diinginkan
-        const deviceLocations = data.results.map(item => ({
-          id: item.id_alat,
-          name: item.nama_alat,
-          address: item.alamat,
-          jarak: item.jarak,
-          rainfall: item.hujan,
-          longitude: item.longitude,
-          latitude: item.latitude
-        }));
-
-        // Menghapus marker yang telah ada sebelumnya
-        for (let i = 0; i < markers.length; i++) {
-          map.removeLayer(markers[i]);
-        }
-        markers = [];
-
-        // memanggil fungsi untuk menampilkan marker di peta
-        showMarkers(deviceLocations);
-
-      } catch (error) {
-        console.error(error);
+      // Menghapus marker yang telah ada sebelumnya
+      for (let i = 0; i < markers.length; i++) {
+        map.removeLayer(markers[i]);
       }
+      markers = [];
 
+      // memanggil fungsi untuk menampilkan marker di peta
+      showMarkers(deviceLocations);
+
+    } catch (error) {
+      console.error(error);
     }
 
-    // fungsi untuk melakukan refresh data setiap 5 detik
-    function refreshData() {
-      setInterval(function () {
-        loadData();
-      }, 3000); // set interval ke 5 detik (5000 ms)
-    }
+  }
 
-    // panggil fungsi refreshData() saat halaman dimuat
-    refreshData();
-  });
+  // fungsi untuk melakukan refresh data setiap 5 detik
+  function refreshData() {
+    setInterval(function () {
+      loadData();
+    }, 3000); // set interval ke 5 detik (5000 ms)
+  }
 
-  map.on('load', function () {
-    document.getElementById('loader').style.display = 'none';
-  });
-}, 3000);
+  // panggil fungsi refreshData() saat halaman dimuat
+  refreshData();
+});
 
 // Fungsi untuk menampilkan SweetAlert
 function showAlert(icon, title, text) {
