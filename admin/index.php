e403049fe85969fb4e59f92874e849439795095c
Arseno <124229977+Arseno11@users.noreply.github.com>
2023-04-16 15:30:25 +0700

    up

--- admin/index.php
+++ admin/index.php
@@ -73,8 +73,8 @@ canvas {
         <div class="sidebar-header position-relative">
           <div class="d-flex justify-content-between align-items-center">
             <div class="logo" style="text-align: center;">
-              <a href="index.php"><img src="assets/images/logo/logo.png" style="width: 40px; height: 40px;" alt="Logo"
-                  srcset=""></a>
+              <a href="index.php"><img src="assets/images/logo/logo.png"
+                  style="width: 40px; height: 40px; padding-bottom: 2px;" alt="Logo" srcset=""></a>
               <br> <span style="font-size: 9px;">SIG BANJIR</span>
             </div>
             <div class="theme-toggle d-flex gap-2  align-items-center mt-2">
