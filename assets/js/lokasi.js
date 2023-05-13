7c082fb723a8ccd26cb9944e29d51ed0ef678842
Arseno <124229977+Arseno11@users.noreply.github.com>
2023-05-14 02:47:26 +0700

    asd

--- assets/js/lokasi.js
+++ assets/js/lokasi.js
@@ -332,11 +332,22 @@ function updateData() {
 
       // Jika terdapat id alat yang error, tampilkan SweetAlert
       if (data.errors) {
-        let errorMessage = 'Data tidak diupdate pada alat dengan Nama: ';
+        let errorMessage = 'Data tidak diupdate pada alat dengan nama: ';
         errorMessage += Object.keys(data.errors).join(', ');
-        showAlert('error', 'Data tidak diupdate', errorMessage, 5000);
+
+        // Cek apakah data masih error berdasarkan localStorage
+        if (localStorage.getItem('dataError') === 'true') {
+          showAlert('error', 'Data tidak diupdate', errorMessage, 5000);
+        }
+
+        // Set localStorage berdasarkan keadaan data
+        if (errorIds.length > 0) {
+          localStorage.setItem('dataError', 'true');
+        } else {
+          localStorage.setItem('dataError', 'false');
+        }
       }
-console.log(data.errors);
+      console.log(data.errors);
       $("#data").html(html);
     })
     .catch(error => {
