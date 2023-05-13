b5243af5839e37e2b7f268476a43e0a7ea662051
Arseno <124229977+Arseno11@users.noreply.github.com>
2023-05-14 02:34:51 +0700

    sad

--- ambildata.php
+++ ambildata.php
@@ -15,19 +15,13 @@ try {
                 // Cek apakah waktu terakhir update melebihi 20 detik dari waktu sekarang
                 if (time() - $lastUpdateTime > 20) {
                         // Jika melebihi 20 detik, tambahkan pesan error ke data
-                        $post['error'] = 'Data tidak diupdate';
+                        $errors[$post['id_alat']] = 'Data tidak diupdate';
                 }
                 $posts[] = $post;
         }
 
-        // Lakukan pengecekan apakah ada pesan error pada data
-        if (!empty($posts)) {
-                foreach ($posts as $post) {
-                        if (isset($post['error'])) {
-                                echo "Error pada alat " . $post['nama_alat'] . ": " . $post['error'] . "<br>";
-                        }
-                }
-        }
+        $data = json_encode(array('results' => $posts, 'errors' => $errors));
+        echo $data;
 } catch (mysqli_sql_exception $e) {
         // Tambahkan pernyataan untuk menangani error dan exception di sini
         echo "Error: " . $e->getMessage();
